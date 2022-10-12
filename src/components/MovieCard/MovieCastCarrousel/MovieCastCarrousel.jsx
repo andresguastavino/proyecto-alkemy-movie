294d77657632d71aa881615de1ea11105369dd7e
Santiago <santiagobalino@hotmail.com>
2022-10-12 00:37:56 -0300

    MovieCastCarrousel component

---
+++ src/components/MovieCard/MovieCastCarrousel/MovieCastCarrousel.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import './styles.css'
+
+export const MovieCastCarrousel = () => {
+  return (
+    <div className='carrousel'>
+      <article>
+        <img src='https://www.themoviedb.org/t/p/w240_and_h266_face/dQTXg59BAmEH6iUHQ3XUjg7drtd.jpg'></img>
+        <h3>Mariska Hargitay</h3>
+        <p>Olivia Benson</p>
+        <p>524 episodios</p>
+      </article>
+      <article>
+        <img src='https://www.themoviedb.org/t/p/w240_and_h266_face/dQTXg59BAmEH6iUHQ3XUjg7drtd.jpg'></img>
+        <h3>Mariska Hargitay</h3>
+        <p>Olivia Benson</p>
+        <p>524 episodios</p>
+      </article>
+      <article>
+        <img src='https://www.themoviedb.org/t/p/w240_and_h266_face/dQTXg59BAmEH6iUHQ3XUjg7drtd.jpg'></img>
+        <h3>Mariska Hargitay</h3>
+        <p>Olivia Benson</p>
+        <p>524 episodios</p>
+      </article>
+      <article>
+        <img src='https://www.themoviedb.org/t/p/w240_and_h266_face/dQTXg59BAmEH6iUHQ3XUjg7drtd.jpg'></img>
+        <h3>Mariska Hargitay</h3>
+        <p>Olivia Benson</p>
+        <p>524 episodios</p>
+      </article>
+      <article>
+        <img src='https://www.themoviedb.org/t/p/w240_and_h266_face/dQTXg59BAmEH6iUHQ3XUjg7drtd.jpg'></img>
+        <h3>Mariska Hargitay</h3>
+        <p>Olivia Benson</p>
+        <p>524 episodios</p>
+      </article>
+      <article>
+        <img src='https://www.themoviedb.org/t/p/w240_and_h266_face/dQTXg59BAmEH6iUHQ3XUjg7drtd.jpg'></img>
+        <h3>Mariska Hargitay</h3>
+        <p>Olivia Benson</p>
+        <p>524 episodios</p>
+      </article>
+    </div>
+  )
+}
