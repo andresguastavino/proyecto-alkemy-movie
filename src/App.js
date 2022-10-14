ab19748687d2a9add8a492eeda5db75bacb655fd
Santiago Guastavino <san.guasta@gmail.com>
2022-10-14 00:11:10 -0300

    init navbar, footer, some style general adjustments

--- src/App.js
+++ src/App.js
@@ -4,26 +4,32 @@ import { RequireAuth } from './components/auth/RequireAuth'
 import { Login } from './components/views/Login/Login'
 import { Register } from './components/views/Register/Register'
 import { MovieCard } from './components/MovieCard/MovieCard'
-
-import './App.css'
+import Layout from './components/Layout'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import './styles.css'
 import Main from './components/main/Main'
 
 export const App = () => {
   return (
     <AuthProvider>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <RequireAuth>
-              <MovieCard />
-            </RequireAuth>
-          }
-        />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/main' element={<Main/>} />
-      </Routes>
+      <Navbar />
+      <Layout>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <RequireAuth>
+                <MovieCard />
+              </RequireAuth>
+            }
+          />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/main' element={<Main/>} />
+        </Routes>
+      </Layout>
+      <Footer />
     </AuthProvider>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './styles.module.css'
+
+export default function Footer () {
+  return (
+    <div className={styles.footerWrapper}>
+      <footer className={styles.footer}>
+        Lorem
+      </footer>
+    </div>
+  )
+}
